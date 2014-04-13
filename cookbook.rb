15fb0ea45f70d5498515c4b01e6ae63b27c2c9fa
Amy Bakameyer <bymak87@gmail.com>
2014-04-14 01:31:48 +0900

    setter and getter methods
    added setter and getter methods

---
+++ cookbook.rb
@@ -0,0 +1,47 @@
+class Cookbook
+	def initialize(title)
+		@title = title
+	end
+
+	def title
+		@title
+	end
+
+	def title=(new_title)
+		@title = new_title
+	end
+
+end
+
+class Recipe
+	def initialize(title, ingredients, steps)
+		@title = title
+		@ingredients = ingredients
+		@steps = steps
+	end
+
+	def title
+		@title
+	end
+
+	def title=(new_title)
+		@title = new_title
+	end
+
+	def ingredients
+		@ingredients
+	end
+
+	def ingredients=(new_ingredients)
+		@ingredients = new_ingredients
+	end
+
+	def steps
+		@steps
+	end
+
+	def steps=(new_steps)
+		@steps = new_steps
+	end
+
+end
